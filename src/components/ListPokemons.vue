c8509d1d1ceeb4d3e06ec3bdeda798af909aa1b8
JrzenonDev <jribertoonb@gmail.com>
2023-08-01 19:29:18 -0300

    feat: passing image as props to ListPokemons component

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -1,7 +1,13 @@
+<script setup>
+const pokemon = defineProps(["name", "urlBaseSvg"]);
+</script>
+>
+
 <template>
-  <div>
-    <div class="card">
-      <p>Nome</p>
+  <div class="col-4">
+    <div class="card p-2 mb-3">
+      <p class="text-center">{{ pokemon.name }}</p>
+      <img :src="urlBaseSvg" class="card-img-top" alt="" height="80" />
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,53 @@
 <script setup>
-  import { onMounted, reactive, ref } from 'vue';
-  import ListPokemons from '../components/ListPokemons.vue'
+import { onMounted, reactive, ref } from "vue";
+import ListPokemons from "../components/ListPokemons.vue";
 
-  let pokemons = reactive(ref());
+let urlBaseSvg = ref(
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
+);
+let pokemons = reactive(ref());
 
-  onMounted(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-      .then(res => res.json())
-      .then(res => pokemons.value = res.results);
-  })
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    .then((res) => res.json())
+    .then((res) => (pokemons.value = res.results));
+});
 </script>
 
 <template>
   <main>
     <div class="container">
       <div class="row mt-5">
-        
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=webp&quality=91&imagick=/wp-content/uploads/2023/02/pikachu_pokemon.jpg" class="card-img-top" alt="...">
+          <div class="card" style="width: 18rem">
+            <img
+              src="https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=webp&quality=91&imagick=/wp-content/uploads/2023/02/pikachu_pokemon.jpg"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
-        
+
         <div class="col-sm-12 col-md-6">
-          <ListPokemons
-            v-for="pokemon in pokemons"
-            :key="pokemon.name"
-            :pokemonName="pokemon.name"
-          />
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemons
+                v-for="pokemon in pokemons"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+              />
+            </div>
+          </div>
         </div>
-      
       </div>
     </div>
   </main>
