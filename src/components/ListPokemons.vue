34504785557f06e10a4a272f6e07960d417d7515
JrzenonDev <jribertoonb@gmail.com>
2023-08-01 19:40:14 -0300

    feat: adding styles hover in card

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -5,9 +5,25 @@ const pokemon = defineProps(["name", "urlBaseSvg"]);
 
 <template>
   <div class="col-4">
-    <div class="card p-2 mb-3">
+    <div class="card p-2 mb-3 cardListPokemon">
       <p class="text-center">{{ pokemon.name }}</p>
       <img :src="urlBaseSvg" class="card-img-top" alt="" height="80" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.cardListPokemon {
+  background: rgb(63, 94, 251);
+  background: radial-gradient(
+    circle,
+    rgba(63, 94, 251, 1) 0%,
+    rgba(252, 70, 107, 0.10970325630252098) 100%
+  );
+  cursor: pointer;
+}
+
+.cardListPokemon img:hover {
+  padding: 0.1rem;
+}
+</style>
