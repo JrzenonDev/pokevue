a9ca5a5a9c7ac74962d48e3d8a646f17b21b2101
JrzenonDev <jribertoonb@gmail.com>
2023-08-01 23:58:29 -0300

    feat: add content in page about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>About page</h1>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card">
+      <h1 class="text-center">PokeVue</h1>
+      <hr />
+      <div class="card-body">
+        <h2>Objetivo</h2>
+        <p>
+          Este é um projeto para fins didáticos utilizando Vue.js 3 com Vite.
+        </p>
+        <p>
+          Link do projeto no Github:
+          <a href="https://github.com/JrzenonDev/pokevue" target="_blank"
+            >https://github.com/JrzenonDev/pokevue</a
+          >
+        </p>
+        <hr />
+        <h2>Recursos utilizados</h2>
+        <p>
+          Neste projeto, foram empregados recursos avançados como a
+          <strong>Composition API</strong>.Além disso, foi realizada a
+          utilização da <strong>Fetch API</strong> para efetuar requisições
+          assíncronas à API de pokemons, buscando e exibindo dados em tempo
+          real.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .container {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
